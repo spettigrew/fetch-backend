1ee2be2c0b81fd35988416122363188f39a7a30f
spettigrew <sara.pettigrew5@gmail.com>
2021-02-19 19:40:21 -0700

    Migration file made with table keys

---
+++ data/migrations/20210219172905_transactions.js
@@ -0,0 +1,14 @@
+
+exports.up = async function (knex) {
+  await knex.schema.createTable('transactions', (table) => {
+      table.increments('id').primary()
+      table.integer('user_id').notNullable()
+      table.string('payer_name').notNullable()
+      table.integer('points')
+      table.datetime('timestamp')
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('transactions')
+};
