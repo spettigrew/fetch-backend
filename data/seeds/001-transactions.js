f4260814e2dd5d7df8922e958b582aef83c9181a
spettigrew <sara.pettigrew5@gmail.com>
2021-02-25 19:56:54 -0700

    Updated seed data for date/time

--- data/seeds/001-transactions.js
+++ data/seeds/001-transactions.js
@@ -2,10 +2,10 @@
 exports.seed = async (knex) => {
   // Deletes ALL existing entries
   await knex('transactions').insert([
-    { user_id: 1, payer_name: "DANNON", points: 1000, timestamp: "2020-11-02T14:00:00Z" },
-    { user_id: 1, payer_name: "UNILEVER", points: 200, timestamp:  "2020-10-31T11:00:00Z" },
-    { user_id: 1, payer_name: "DANNON", points: -200, timestamp: "2020-10-31T15:00:00Z" },
-    { user_id: 1, payer_name: "MILLER COORS", points: 10000, timestamp: "2020-11-01T14:00:00Z" },
-    { user_id: 1, payer_name: "DANNON", points: 300, timestamp: "2020-10-31T10:00:00Z" }
+    { user_id: 1, payer_name: "DANNON", points: 1000, timestamp: new Date() },
+    { user_id: 1, payer_name: "UNILEVER", points: 200, timestamp: new Date() },
+    { user_id: 1, payer_name: "DANNON", points: -200, timestamp: new Date() },
+    { user_id: 1, payer_name: "MILLER COORS", points: 10000, timestamp: new Date() },
+    { user_id: 1, payer_name: "DANNON", points: 300, timestamp: new Date() }
   ])
 };
