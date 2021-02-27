3eb97874654c1c682ca42d98ba61d838c3717777
spettigrew <sara.pettigrew5@gmail.com>
2021-02-26 19:16:04 -0700

    Get balance router set up

--- transactions/transactions-router.js
+++ transactions/transactions-router.js
@@ -7,13 +7,15 @@ const router = express.Router()
 
 router.get("/balances", async (req, res, next) => {
     try {
-        return res.json(await Transactions.getBalance())
+        const result = await Transactions.getBalance()
+        if (result) {
+            res.status(200).json(result)
+        }
     }
     catch (err) {
         next(err)
     }
 })
-
 
 router.post("/", async (req, res, next) => {
     try {
@@ -24,34 +26,28 @@ router.post("/", async (req, res, next) => {
     }
 })
 
+router.get('/', async ( req, res, next ) => {
+    try {
+        const result = await Transactions.getAll()
+        if (result) {
+            res.status(200).json(result)
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
 router.put("/spend-points", async (req, res, next) => {
  try {
     const { points } = req.body
     const result = await Transactions.spendPoints(points)
-    res.status(200).json(result)
+    if (result) {
+        res.status(200).json(result)
+    }
 
     } catch (err) {
             next(err)
         }
     }) 
+
 module.exports = router
-
-
-
-// async function put(req, res, next) {
-//     try {
-//         let employee = getEmployeeFromRec(req);
-
-//         employee.employee_id = parseInt(req.params.id, 10);
-
-//         employee = await employees.update(employee);
-
-//         if (employee !== null) {
-//             res.status(200).json(employee);
-//         } else {
-//             res.status(404).end();
-//         }
-//     } catch (err) {
-//         next(err);
-//     }
-// }
